e3385d3e84fb8a362b4984e5d780d209c05fd9ac
fbudiman <fbudiman14@hotmail.com>
2019-02-05 22:26:48 -0800

    some style tweaks

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const columns = document.getElementsByClassName('column')
 
-const repeatIdx = idx => idx >= 4 ? idx % 4 : idx
+const colIndex = idx => idx >= 4 ? idx % 4 : idx
 
 $(document).ready(() => {
 
-dogs.forEach(({ image, source, name }, idx) => {
- 	$(columns[repeatIdx(idx)]).append(`
-  		<div class="container">
-   			<span class="name">${name}</span>
-   			<img src=.${image} />
-  		</div>`)
+	dogs.forEach(({ image, source, name }, idx) => {
+	 	$(columns[colIndex(idx)]).append(`
+	  		<div class="container">
+	   			<span class="name">${name}</span>
+	   			<img src=.${image} />
+	  		</div>`)
 	})
 
 })
