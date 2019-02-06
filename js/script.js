ad79845947615d64b424018dfc8fae6f10ee1d31
fbudiman <fbudiman14@hotmail.com>
2019-02-05 23:17:18 -0800

    starts setting up selection logic and more style tweaks

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
+
+let selected
+
 const columns = document.getElementsByClassName('column')
 
 const colIndex = idx => idx >= 4 ? idx % 4 : idx
 
+const setSelected = img => {
+	selected = img
+}
+
 $(document).ready(() => {
 
-	dogs.forEach(({ image, source, name }, idx) => {
-	 	$(columns[colIndex(idx)]).append(`
-	  		<div class="container">
-	   			<span class="name">${name}</span>
-	   			<img src=.${image} />
-	  		</div>`)
-	})
+	const appendPuppies = () => {
+		dogs.forEach(({ image, source, name }, idx) => {
+		 	$(columns[colIndex(idx)]).append(`
+		  		<div class="container">
+		   			<span class="name">${name}</span>
+		   			<img src=.${image} onclick=setSelected(\`.${image}\`) />
+		  		</div>`)
+		})
+	}
+
+	appendPuppies()
 
 })
